47a28af0aef87b851d6ac0108bd2555b81d53d37
Peter Gray <peter@petergraycreative.com>
2018-03-15 16:09:11 -0500

    Added in a new routes to characters and films
    Can now get character by id and populate film data.

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -4,6 +4,18 @@ const Character = require('./Character.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.get('/:id', (req, res) => {
+  const charId = req.params.id;
+  Character.find({_id: req.params.id})
+  .populate('homeworld')
+  .then(character => {
+    res.status(200).json(character);
+  })
+})
+
+router.get('/:id/vehicles', (req, res) => {
+  const charId = req.params.id;
+  
+})
 
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -7,21 +7,26 @@ const router = express.Router();
 // add endpoints here
 router.get('/', function(req, res) {
   const producerFilter = req.query.producer;
+  const releasedFilter =  req.query.released;
 
   let query = Film.find({})
     .sort('episode')
-    // .populate('characters planets species')
-    .select('title producer');
-  // .populate('characters', 'name gender height skin_color')
-  // .populate('planets', 'name climate terrain gravity diameter');
+    .select('title producer')
+    .populate(
+      'characters',
+      '_id name gender height skin_color hair_color eye_color'
+    )
+    .populate('planets', 'name climate terrain gravity diameter');
 
   if (producerFilter) {
-    query.where({ producer: /gary kurtz/i });
+    query.where({ producer: { $regex: new RegExp(producerFilter, 'i') } });
+  }
+  if(releasedFilter) {
+    query.where({ release_date: { $regex: new RegExp(releasedFilter, 'i') } });
   }
 
-  query.then(films => {
+  query.then((films) => {
     res.json(films);
   });
 });
-
 module.exports = router;
